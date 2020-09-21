b36bf0c9b70cbacde4a2b9c22c9855202f4ad557
edisonhello <edisonhello@hotmail.com>
2020-09-21 23:02:29 +0800

    Add some router part

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/brianbbsu/ioicamp-backend
 go 1.15
 
 require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/mattn/go-sqlite3 v1.14.3
 	github.com/spf13/viper v1.7.1
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"bytes"
+	"database/sql"
 	"fmt"
+	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var defaultConfig = []byte(`
@@ -18,6 +22,9 @@ TemplateDir: ./template/
 
 // Config is the config for the whole package
 var Config *viper.Viper
+
+var port int = 3030
+var userDb *sql.DB
 
 func initConfig() {
 	Config = viper.New()
@@ -42,4 +49,95 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Done!")
+
+	initServer()
+}
+
+func initServer() {
+	router := gin.Default()
+
+	initRouter(router)
+	initDatabase()
+
+	router.Run(fmt.Sprint("localhost:", port))
+}
+
+func initRouter(router *gin.Engine) {
+	router_initApiRouter(router.Group("/api"))
+}
+
+func initDatabase() {
+	var err error
+
+	userDb, err = sql.Open("sqlite3", "../test.db")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	userDb.Exec("create table users (username varchar(20) not null, password varchar(20) not null, email varchar(20) not null, primary key (username));")
+}
+
+// router
+
+func router_initApiRouter(group *gin.RouterGroup) {
+	router_api_initUserRouter(group.Group("/users"))
+
+	group.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H {
+			"message": "pong",
+		})
+	})
+}
+
+func router_api_initUserRouter(group *gin.RouterGroup) {
+	group.POST("/login", controller_users_login)
+
+}
+
+// controller
+
+func controller_users_login(c *gin.Context) {
+	var user User
+
+	c.BindJSON(&user)
+
+	log.Println(user.Username, user.Password)
+	stmt, err := userDb.Prepare("select * from users where username = ? and password = ?")
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	
+	rows, err := stmt.Query(user.Username, user.Password)
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&user.Username, &user.Password, &user.Email)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	c.JSON(200, gin.H {
+		"status":  "success",
+		"username": user.Username,
+		"password": user.Password,
+		"email": user.Email,
+	})
+}
+
+// model
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email string `json:"email"`
 }
